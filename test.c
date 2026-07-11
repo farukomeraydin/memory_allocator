8da6ceb9404115ba6529a6b515ed01aae238e3ed
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2026-07-11 03:23:32 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,4 +1,28 @@
+```c
 #include "mm.h"
+
 #include <stdio.h>
 #include <string.h>
-int main(void){char*a=mm_malloc(32);strcpy(a,"allocator works");puts(a);int*b=mm_calloc(100,sizeof(int));b=mm_realloc(b,200*sizeof(int));mm_free(a);mm_free(b);mm_dump();return 0;}
+
+int main(void)
+{
+    char *a = mm_malloc(32);
+
+    strcpy(a, "allocator works");
+    puts(a);
+
+    int *b = mm_calloc(100, sizeof(int));
+
+    b = mm_realloc(
+        b,
+        200 * sizeof(int)
+    );
+
+    mm_free(a);
+    mm_free(b);
+
+    mm_dump();
+
+    return 0;
+}
+```
