28e244b753e69c62161835ab78187255c22d3644
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2026-07-11 03:22:38 +0300

    Update mm.h

--- mm.h
+++ mm.h
@@ -1,5 +1,12 @@
 #ifndef MM_H
 #define MM_H
+
 #include <stddef.h>
-void*mm_malloc(size_t n);void*mm_calloc(size_t c,size_t n);void*mm_realloc(void*p,size_t n);void mm_free(void*p);void mm_dump(void);
+
+void*mm_malloc(size_t n);
+void*mm_calloc(size_t c,size_t n);
+void*mm_realloc(void*p,size_t n);
+void mm_free(void*p);
+void mm_dump(void);
+
 #endif
